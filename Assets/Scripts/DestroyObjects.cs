8ba35191b72f2d99a20e77d29f3287d19fa26c00
yahyademirbas <yahyademirbas@yahoo.com>
2021-04-09 22:06:21 +0300

    desstroy

---
+++ Assets/Scripts/DestroyObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestrotObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    BoxCollider boxCollider;
+    void Start()
+    {
+        boxCollider = GetComponentInChildren<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
